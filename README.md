9cd5abf8bdc6dde1b4f72548817d498316ea6ba8
Stefan Boos <kontakt@boos.systems>
2022-10-26 21:12:29 +0200

    initial repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tampermonkey Scripts
+
+This repository contains Userscripts for use with [Tampermonkey](https://tampermonkey.net).
---
+++ src/hello-world.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Hello World
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://xkcd.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("hello, world!");
+})();
